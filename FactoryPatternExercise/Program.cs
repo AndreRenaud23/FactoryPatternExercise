ccf57510f8bb5bc0a0499155d2015f1ed9b2aaad
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-29 11:59:28 -0500

    First commit

---
+++ FactoryPatternExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿using FactoryPatternExercise;
+
+Console.WriteLine("How many wheels would you like your vehicle have?");
+
+var answer = Console.ReadLine();
+
+VehicleFactory setter = new VehicleFactory();
+
+IVehicle vehicle = setter.GetVehicle(answer);
+
+vehicle.Drive();
---
+++ FactoryPatternExercise/VehicleFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    internal class VehicleFactory
+    {
+        public IVehicle GetVehicle(string numOfWheels)
+        {
+            
+            switch (numOfWheels.ToLower())
+            {
+                case "two":
+                    return new Motorcycle();
+                case "2":
+                    return new Motorcycle();
+                case "4":
+                    return new Car();
+                case "four":
+                    return new Car();
+                default:
+                    Console.WriteLine("Not recognisable");
+                    return new Car();
+                    
+            }
+
+        }
+    }
+}
